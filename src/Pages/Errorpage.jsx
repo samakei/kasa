7ec7673aba0f3b988ab3bf0d4461871231c958f9
SAMAKE Issa <samakeissa10@outlook.fr>
2024-01-25 12:01:38 +0100

    ajout du modification

--- src/Pages/Errorpage.jsx
+++ src/Pages/Errorpage.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import "../Styles/errorpage.scss"
-import { NavLink } from 'react-router-dom'
-import Header from '../Layout/Header'
-import Footer from '../Layout/Footer'
+import { NavLink } from 'react-router-dom';
+import Header from "../Layout/Header";
+import Footer from "../Layout/Footer";
+
 
 export default function Errorpage() {
   return (
     <div className='eurrepage '>
          <>
-        <Header />
+         <Header /> 
         <h1 className='eurrer404'>404</h1>
         <h2>Oups! La page que  vous demandez n'existe pas.</h2>
 
@@ -17,8 +18,7 @@ export default function Errorpage() {
             <p>Retournez sur la page d'accueil</p>
             </NavLink>
           </div>
-         
-        <Footer />
+          <Footer />
         </>
     </div>
   )
